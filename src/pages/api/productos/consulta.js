ae8b66e9e0b008a746db7e6361b9b44703ab10d2
Ivan Gordillo <gordillosolis@icloud.com>
2025-03-03 09:00:17 -0600

    Fix: :sad dislexia

--- src/pages/api/productos/consulta.js
+++ src/pages/api/productos/consulta.js
@@ -6,15 +6,16 @@ export const GET = async ({ params, request }) => {
     const productos = await readJSON(FILE)
     console.log(productos)
 
-    if (productos.lenght != 0) {
-        return new Response(JSON.stringify(productos), { status: 200 })
-    }
+    if (productos.length === 0) {
 
-    return new Response(JSON.stringify({
-        message: "No se encontraron productos",
-    }),
+        return new Response(JSON.stringify({
+            message: "No se encontraron productos",
+        }),
         {
             status: 404,
         })
+    }
+    
+    return new Response(JSON.stringify(productos), { status: 200 })
 
 }
