9515e2b73539edafa4395cd86dda0b0d0c933d0b
Ivan Gordillo <gordillosolis@icloud.com>
2025-04-07 20:18:19 -0600

    Fix(Linter): El linter que se pone de nena

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 import vercel from '@astrojs/vercel'
 
-import db from '@astrojs/db';
+import db from '@astrojs/db'
 
 // https://astro.build/config
 export default defineConfig({
